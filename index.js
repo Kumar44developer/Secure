7f848187629fb2078338c10cfaee6f8a00ba45be
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-13 12:47:05 +0530

    Queried database to find user by email during login process

--- index.js
+++ index.js
@@ -71,6 +71,10 @@ app.post("/login", async (req, res) => {
   const loginPassword = req.body.password;
 
 
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
 
 
